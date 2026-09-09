6dba9db80a7adb66449759f15bad75637fc08b8d
Sanveer Gill <sanveergill01@gmail.com>
2026-09-09 01:03:56 -0400

    Set up Spring Boot backend with PostgreSQL

---
+++ backend/src/main/java/com/sanveer/banking_platform_backend/BankingPlatformBackendApplication.java
@@ -0,0 +1,14 @@
+package com.sanveer.banking_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingPlatformBackendApplication.class, args);
+		System.out.println("Banking Platform Backend Application started successfully.");
+	}
+
+}
---
+++ backend/src/test/java/com/sanveer/banking_platform_backend/BankingPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanveer.banking_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
