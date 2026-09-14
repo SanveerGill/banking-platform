f07ad9f9a6fbf5698a498316fa2cc649c090d639
Sanveer Gill <sanveergill01@gmail.com>
2026-09-13 23:25:45 -0400

    added validations for CreateUserRequest DTO fields

--- backend/src/main/java/com/sanveer/banking_platform_backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/sanveer/banking_platform_backend/exceptions/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package com.sanveer.banking_platform_backend.exceptions;
 import com.sanveer.banking_platform_backend.dtos.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,5 +25,22 @@ public class GlobalExceptionHandler {
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(error.getField(), error.getDefaultMessage())
+                );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errors);
     }
 }
